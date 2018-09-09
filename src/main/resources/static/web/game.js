c8c0252cb0d3215328b9245e3b5cc0894b1e78ed
Moria Habte <moriahabte@gmail.com>
2018-09-09 19:10:29 +0200

    shipFlip

--- src/main/resources/static/web/game.js
+++ src/main/resources/static/web/game.js
@@ -1,74 +1,75 @@
+var shipType = "";
 
+function drag(ev) {
+    shipType = ev.target.className;
+    ev.dataTransfer.setData("text", ev.target.id);
+}
 
 function nextChar(c) {
     return String.fromCharCode(c.charCodeAt(0) + 1);
 }
+
 function formerChar(c) {
     return String.fromCharCode(c.charCodeAt(0) - 1);
 }
+
 function allowDrop(ev) {
-    
+    var shipLength = document.getElementsByClassName(shipType).length;
     var t = event.target;
+    var b = event.target.id;
+    var elementIdNumber = parseInt(b.split(/(?=[1-9])/)[1], 10);
+    var elementIdName = b.split(/(?=[1-9])/)[0];
+    var dropable = true;
     // Find the drop target
     while (t !== null && !t.classList.contains("target")) {
         t = t.parentNode;
     }
-    // If the target is empty allow the drop.
+    // If the targets are empty allow the drop.
     if (t && t.childNodes.length == 0) {
-        event.preventDefault();
+        for (var i = 1; i < shipLength; i++) {
+            if (app.rotateShip1 == false) {
+                if (document.getElementById(elementIdName + (elementIdNumber + i)).childNodes.length != 0) {
+                    dropable = false;
+                }
+            } else {
+                if (document.getElementById(String.fromCharCode(elementIdName.charCodeAt(0) + i) + elementIdNumber).childNodes.length != 0) {
+                    dropable = false;
+                }
+            }
+        }
+        if (dropable) {
+            event.preventDefault();
+        }
     }
     return false;
-//    ev.preventDefault();
-    
-}
-
-function drag(ev) {
-    
-    ev.dataTransfer.setData("text", ev.target.id);
 }
 
 function drop(ev) {
-    
+    var b = ev.target.childNodes
     ev.preventDefault();
     var data = ev.dataTransfer.getData("text");
-    
-   
-   var elementIdNumber =  parseInt(data.split(/(?=[1-9])/)[1], 10);
-    var elementIdName =data.split(/(?=[1-9])/)[0];
-    console.log(elementIdName);
-    console.log(elementIdNumber);
+    var elementIdNumber = parseInt(data.split(/(?=[1-9])/)[1], 10);
+    var elementIdName = data.split(/(?=[1-9])/)[0];
     ev.target.appendChild(document.getElementById(data));
-   
-    
     var letterNumberArray = ev.target.id.split(/(?=[1-9])/);
-    console.log(letterNumberArray[0] );
-    
     var number = letterNumberArray[1];
     app.letter = letterNumberArray[0];
-//    console.log(number);
-app.integer = parseInt(number, 10);
-    
-//    console.log(app.integer + 1);
-    console.log(letterNumberArray[0]);
-    
-    if(app.rotateShip1 == false){
+    app.integer = parseInt(number, 10);
+    var ships = document.getElementsByClassName(shipType);
+    var shipLength = document.getElementsByClassName(shipType).length;
 
-        if(document.getElementById(app.letter + (app.integer+1)) != null){
-           document.getElementById(app.letter + (app.integer+1)).appendChild(document.getElementById(elementIdName + (elementIdNumber + 1)));
-           }else{
-               document.getElementById(app.letter + (app.integer-1)).appendChild(document.getElementById(elementIdName + (elementIdNumber + 1)));
-           }
-        
-    }else{
-        if(document.getElementById(nextChar(app.letter) + (app.integer)) != null){
-             document.getElementById(nextChar(app.letter) + (app.integer)).appendChild(document.getElementById(elementIdName + (elementIdNumber + 1)));
-        }else{
-            document.getElementById(formerChar(app.letter) + (app.integer)).appendChild(document.getElementById(elementIdName + (elementIdNumber + 1)));
+    console.log(shipType);
+    console.log(shipLength);
+
+    for (var i = 0; i < shipLength; i++) {
+
+        if (app.rotateShip1 == false) {
+            document.getElementById(app.letter + (app.integer + i)).appendChild(document.getElementById(elementIdName + (elementIdNumber + i)));
+        } else {
+            document.getElementById(String.fromCharCode(app.letter.charCodeAt(0) + i) + (app.integer)).appendChild(document.getElementById(elementIdName + (elementIdNumber + i)));
         }
-        
-    }
 
-    
+    }
 }
 
 
@@ -86,6 +87,7 @@ var app = new Vue({
         rotateShip1: false,
         letter: 0,
         integer: 0,
+        dropable: true,
     },
     created: function () {
         //        this.postShips();
@@ -116,15 +118,6 @@ var app = new Vue({
                     app.userIsPlayer1(data);
 
 
-                    //make shipLocations td turn purple with ship class
-                    //                app.ShowShipLocations();
-                    //
-                    //                //make salvolocations td turn green with salvo class
-                    //                app.showSalvolocations();
-                    //
-                    //                /*make opponenSalvolocations td turn orange with opponentSalvo
-                    //                make shipLocation that got hit purlple with yougothit*/
-                    //                app.showOpponenSalvolocations();
                 });
 
         },
@@ -150,49 +143,19 @@ var app = new Vue({
             shipLocations = [];
             console.log(wang);
 
-            //            for (let i = 0; i < wang.length; i++) {
-            //                shipLocations = data.ships[i].location;
-            //                console.log(shipLocations);
-            //                for (let j = 0; j < shipLocations.length; j++) {
-            //                    document.getElementById(data.ships[i].location[j]).classList.add("ship");
-            //                }
-            //            }
-
 
         },
         showSalvolocations: function () {
             raw = data.salvoes;
             console.log(raw);
 
-            //            for (let i = 0; i < raw.length; i++) {
-            //                salvoLocation = data.salvoes[i].locations;
-            //                console.log(salvoLocation);
-            //                app.salvoTurn = data.salvoes[i].turn;
-            //                for (let j = 0; j < salvoLocation.length; j++) {
-            //                    document.getElementById("s" + data.salvoes[i].locations[j]).classList.add("salvo");
-            //                }
-            //            }
+
         },
         showOpponenSalvolocations: function () {
             opponentSalvoes = data.opponentSalvoes;
             console.log(opponentSalvoes);
 
-            //            for (let i = 0; i < opponentSalvoes.length; i++) {
-            //
-            //                opponentSalvoLocation = data.opponentSalvoes[i].locations;
-            //                console.log(opponentSalvoLocation);
-            //                app.salvoTurn2 = data.opponentSalvoes[i].turn;
-            //
-            //                for (let j = 0; j < opponentSalvoLocation.length; j++) {
-            //                    let cell = document.getElementById(opponentSalvoLocation[j]);
-            //                    if (cell.classList.contains("ship")) {
-            //                        cell.classList.remove('ship')
-            //                        cell.classList.add('youGotHit')
-            //                    } else {
-            //                        cell.classList.add("opponentSalvo");
-            //                    }
-            //                }
-            //            }
+
         },
         logOut: function () {
             fetch("/api/logout", {
@@ -221,53 +184,127 @@ var app = new Vue({
             history.back();
         },
         postShips: function () {
-            fetch("/api/games/players/" + this.id + "/ships", {
-                    credentials: 'include',
-                    method: 'POST',
-                    headers: {
-                        'Accept': 'application/json',
-                        'Content-Type': 'application/json'
-                    },
+            post = true;
+            shipDock = document.getElementsByClassName("div1").length;
 
-                    body: JSON.stringify([{
-                            shipType: "battleship",
-                            shipLocation: ["A1", "A6", "A8"]
+            for (i = 0; i < shipDock; i++) {
+                if (document.getElementsByClassName("div1")[i].childElementCount != 0) {
+                    post = false;
+                }
+            }
+
+            if (post) {
+
+                battleShipLocation = [];
+                battleShipLength = document.getElementsByClassName("battleship").length;
+                
+                for(i=0;i<battleShipLength;i++){
+                    
+                }
+                
+
+                fetch("/api/games/players/" + this.id + "/ships", {
+                        credentials: 'include',
+                        method: 'POST',
+                        headers: {
+                            'Accept': 'application/json',
+                            'Content-Type': 'application/json'
+                        },
+
+                        body: JSON.stringify([
+                            {
+                                shipType: "battleship",
+                                shipLocation: ["A1", "A6", "A8"]
                },
-                        {
-                            shipType: "oilship",
-                            shipLocation: ["B1", "B6", "B8", "B9"]
+                            {
+                                shipType: "oilship",
+                                shipLocation: ["B1", "B6", "B8", "B9"]
                },
                                          ])
-                })
-                .then(r => r.json())
-                .then(r => {
-                    console.log(r);
-                    location.reload();
+                    })
+                    .then(r => r.json())
+                    .then(r => {
+                        console.log(r);
+                        location.reload();
 
-                })
-                .catch(r => console.log(r))
+                    })
+                    .catch(r => console.log(r))
+            }
         },
-         turnShip1: function(){
-             
-    if(app.rotateShip1 == true){
-        app.rotateShip1 = false;
-        console.log("false")
-         if(document.getElementById(this.letter + (app.integer+1)) != null){
-           document.getElementById(this.letter + (app.integer+1)).appendChild(document.getElementById("drag2"));
-           }else{
-               document.getElementById(this.letter + (app.integer-1)).appendChild(document.getElementById("drag2"));
-           }
-        
-    }else{
-        app.rotateShip1 = true;
-        console.log("true");
-        if(document.getElementById(nextChar(this.letter) + (app.integer)) != null){
-             document.getElementById(nextChar(this.letter) + (app.integer)).appendChild(document.getElementById("drag2"));
-        }else{
-            document.getElementById(formerChar(this.letter) + (app.integer)).appendChild(document.getElementById("drag2"));
-        }
-    }
-},
+        turnShip1: function (id) {
+            //            data = ev.dataTransfer.getData("text");
+            elementIdNumber = parseInt(id.split(/(?=[1-9])/)[1], 10);
+            elementIdLetter = id.split(/(?=[1-9])/)[0];
+            full = console.log(document.getElementById(id).parentNode.id);
+            letter = document.getElementById(id).parentNode.id.split(/(?=[1-9])/)[0];
+
+            shipLength = document.getElementsByClassName(shipType).length;
+
+            number = document.getElementById(id).parentNode.id.split(/(?=[1-9])/)[1];
+            integer = parseInt(number.split(/(?=[1-9])/)[0], 10);
+
+            app.dropable = true;
+
+
+
+
+
+
+            if (app.rotateShip1 == true) {
+                for (var i = 1; i < shipLength; i++) {
+                    console.log(document.getElementById(letter + (integer + i)).childNodes.length);
+                    if (document.getElementById(letter + (integer + i)).childNodes.length !== 0) {
+                        app.dropable = false;
+                        console.log("flipped vertical")
+                    }
+                }
+            } else if (app.rotateShip1 == false) {
+                for (var i = 1; i < shipLength; i++) {
+                    if (document.getElementById(String.fromCharCode(letter.charCodeAt(0) + i) + (integer)).childNodes.length !== 0) {
+                        console.log("whadup");
+                        app.dropable = false;
+                    }
+                }
+            }
+            console.log(app.dropable);
+
+            if (app.dropable == true) {
+
+                if (app.rotateShip1 == false) {
+
+
+                    for (var i = 1; i < shipLength; i++) {
+                        document.getElementById(String.fromCharCode(letter.charCodeAt(0) + i) + (integer)).appendChild(document.getElementById(elementIdLetter + (elementIdNumber + i)));
+                    }
+                    app.rotateShip1 = true;
+                } else {
+
+
+                    for (var i = 1; i < shipLength; i++) {
+                        document.getElementById(letter + (integer + i)).appendChild(document.getElementById(elementIdLetter + (elementIdNumber + i)));
+                    }
+                    app.rotateShip1 = false;
+                }
+            }
+
+
+
+
+        },
+        showPosition: function () {
+            post = true;
+            shipDock = document.getElementsByClassName("div1").length;
+
+            for (i = 0; i < shipDock; i++) {
+                if (document.getElementsByClassName("div1")[i].childElementCount != 0) {
+                    post = false;
+                }
+            }
+            console.log(post);
+
+
+
+        },
 
 
     }
